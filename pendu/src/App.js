e1a4d5528fbb3c368fb9c586dfd853b83067836e
Pierre Pellegrini <pellegrini.pierre@gmail.com>
2020-09-11 06:39:07 +0200

    Code cleaning again

--- pendu/src/App.js
+++ pendu/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+const VISUAL_PAUSE_MSECS = 750
+
+class App extends Component {
+  state = {
+  }
+
+  render() {
+    return (
+      <div className="memory">
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default App
